78aa8a239b9c7152d24df0e3ad2c3ddab4f45472
Edward <edward.heaver01@gmail.com>
2021-03-17 22:23:26 +0000

    set up requesting state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './style';
 import { useState } from 'preact/hooks'
+import Bars from './assets/Bars.svg'
 
 export default function App() {
 
 	const [text, settext] = useState("");
-	const [requesting, setrequesting] = useState(false);
+	const [requesting, setrequesting] = useState(true);
 
 	const handleOnChange = (event) => {
 		settext(event.target.value);
@@ -15,19 +16,26 @@ export default function App() {
 	}
 
 	return (
-		<main>
-			<h1>Very much translate the English</h1>
-
-			<div>{ requesting ? 
-				<form class="form__area">
-					<label>Type your text here</label>
-					<textarea class="form__area__text" value={text} onChange={event => handleOnChange(event)} />
-					<button class="form__area__button" onClick={event => handleOnSubmit(event)}>
-						<b>Make this Dunglish</b>
-					</button>
-				</form>
-				}				
-			</div>
-		</main>
+		<fragment>
+			<header>
+				<h1>Very much translate the English</h1>
+			</header>
+			<main>
+				<div>{ requesting ? 
+					<img src={Bars} /> :
+					<form class="form__area">
+						<label>Type your text here</label>
+						<textarea class="form__area__text" value={text} onChange={event => handleOnChange(event)} />
+						<button class="form__area__button" onClick={event => handleOnSubmit(event)}>
+							<b>Make this Dunglish</b>
+						</button>
+					</form>
+					}				
+				</div>
+			</main>
+			<footer>
+				<p>this is my footer</p>
+			</footer>
+		</fragment>
 	);
 }
