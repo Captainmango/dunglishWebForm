bd25814321e204fa8b3460f1eb7886b396887c35
Edward <edward.heaver01@gmail.com>
2021-03-17 22:17:01 +0000

    added loading svg

--- src/index.js
+++ src/index.js
@@ -4,21 +4,29 @@ import { useState } from 'preact/hooks'
 export default function App() {
 
 	const [text, settext] = useState("");
+	const [requesting, setrequesting] = useState(false);
 
 	const handleOnChange = (event) => {
 		settext(event.target.value);
 	}
 
+	const handleOnSubmit = (event) => {
+		event.preventDefault();
+	}
+
 	return (
 		<main>
-			<div>
+			<h1>Very much translate the English</h1>
+
+			<div>{ requesting ? 
 				<form class="form__area">
 					<label>Type your text here</label>
 					<textarea class="form__area__text" value={text} onChange={event => handleOnChange(event)} />
-					<button class="form__area__button">
+					<button class="form__area__button" onClick={event => handleOnSubmit(event)}>
 						<b>Make this Dunglish</b>
 					</button>
 				</form>
+				}				
 			</div>
 		</main>
 	);
